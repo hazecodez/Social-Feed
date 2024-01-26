9945f2b4e337f5fbf9e96cbba0a5ca98ff00a6b9
hazecodez <muhammedhashir298@gmail.com>
2024-01-26 19:36:00 +0530

    Server

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const app = express()
+const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("Hello World!!")
-})
+app.get('/',(req,res)=> res.send('Hello Social Feed'));
 
-app.listen(3000,()=> console.log('Running'))
+
+app.listen(3000,()=> console.log('Server Running'));
